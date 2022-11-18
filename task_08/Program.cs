28830560f3e87c46c7af65f798ee8d7e4d0f947e
leinore <116540768+leinore@users.noreply.github.com>
2022-11-18 13:44:41 +0300

    add task_08

---
+++ task_08/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = -number; i < number+1; i++)
+{
+    if (i%2 == 0 )
+    {
+        Console.WriteLine(i);
+    }
+}
